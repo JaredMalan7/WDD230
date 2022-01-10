64e685bf7e5e8e09cde22cce742600c8467b1115
JaredMalan7 <jared.malan7@gmail.com>
2022-01-10 00:29:04 -0700

    latest update

--- js/dates.js
+++ js/dates.js
@@ -1,2 +1,11 @@
+const d = new Date();
+const year = d.getFullYear();
+
+
+//using get ElementById
+//document.querySelector("#currentyear").textContent = year;
+//document.getElementById("lastupdated").textContent = "Last Updated: " + document.lastModified;   (Professor prefers us to use a template literal insteda of concatenation)
+
+
 const lastmod = document.querySelector('#lastmod');
-lastmod.innerHTML = `Date the <strong>document</strong> was last modified: ${document.lastModified}`;
+lastmod.innerHTML = `Last Updated: ${document.lastModified}`;
