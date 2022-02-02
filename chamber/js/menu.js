9e9e1386d1eed2a66076d3fa5e7864acc4c6ff8a
JaredMalan7 <jared.malan7@gmail.com>
2022-02-01 21:31:53 -0700

    date problem fixed

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -9,7 +9,7 @@ menubutton.addEventListener('click', () => {
     hamburger.classList.toggle('hidden');
 }, false);
 
-// To solve the mid resizing issue with responsive class on
+
 window.onresize = () => {
     if (window.innerWidth > 760) {
         mainnav.classList.remove('responsive');
