cca96d6704e7aa6dc70deabd770c76b8fc64d2bb
JaredMalan7 <jared.malan7@gmail.com>
2022-02-07 01:00:55 -0700

    update

---
+++ chamber/images/Utah Mountain Chamber _ Home_files/dates.js
@@ -0,0 +1,14 @@
+
+
+document.querySelector("#year").textContent = new Date().getFullYear();
+document.querySelector("#lastmod").textContent = `Last Updated: ${document.lastModified}`;
+
+const datefieldUK = document.querySelector(".current-date");
+
+
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"}).format(now);
+
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
