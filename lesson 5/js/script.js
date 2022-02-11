e1c3b64932d2170c688b9049895281ef9c933ff3
JaredMalan7 <jared.malan7@gmail.com>
2022-02-10 19:01:25 -0700

    search button fix

--- lesson 5/js/script.js
+++ lesson 5/js/script.js
@@ -20,6 +20,8 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector(".list");
 
+button.setAttribute("disabled", true);
+
 button.addEventListener("click", function() {
     const li = document.createElement("li");
     const button2 = document.createElement("button");
@@ -33,6 +35,7 @@ button.addEventListener("click", function() {
 
 })
 
+
 function deleteItem (){
    const lipadre = (this).parentNode.remove();
    input.focus();
@@ -42,3 +45,11 @@ function cleanInput(){
     input.focus();
     input.value = ""
 }
+
+input.addEventListener("input", function () {
+    if (input.value != '' && input.value != null )
+        button.removeAttribute("disabled");
+    else
+        button.setAttribute("disabled", true);
+});
+
