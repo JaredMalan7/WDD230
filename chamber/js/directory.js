8ab9361cd92858e5ea4f2023563fcfa9c55e8bf3
JaredMalan7 <jared.malan7@gmail.com>
2022-03-05 22:21:07 -0700

    Json updates

---
+++ chamber/js/directory.js
@@ -0,0 +1,5 @@
+const requestURL = "data.json"
+const cards = document.querySelector('div.cards-view');
+const listButton = document.querySelector("#list-button");
+const cardButton = document.querySelector("#grid-button");
+
