03beaf98584652b4f6e82752dc7623933681d4fe
JaredMalan7 <jared.malan7@gmail.com>
2022-03-14 20:05:56 -0600

    commit weather

--- lesson 10/js/weather-api.js
+++ lesson 10/js/weather-api.js
@@ -1,5 +1,24 @@
 const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=f0c0d0a7b7dce39bfd0195af70ad1025";
-// const apiURL = "api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}";
+
+fetch( apiURL )
+    .then( response => response.json())
+    .then( data => {
+        if( data.cod == 401 || data.cod == 404 )
+            console.log(`ERROR (#${data.cod}): ${data.message}`);
+        else {
+            //console.log(data);
+            const iconSrc = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png';
+            const desc = data.weather[0].description;
+            
+            document.querySelector('#temp').textContent = data.main.temp.toFixed(1);
+            document.querySelector('.wheater-imge > img').setAttribute('src', iconSrc);
+            document.querySelector('.wheater-imge > img').setAttribute('alt', desc);
+            document.querySelector('#wheater-api').textContent = desc;
+            document.querySelector("#speed").textContent = data.wind.speed;
+            
+        }
+    });
+
 
 fetch(apiURL)
   .then((response) => response.json())
