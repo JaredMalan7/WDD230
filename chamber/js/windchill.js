42ae9a0e640b9fef60c0087eb5c3fd61cec1daf7
JaredMalan7 <jared.malan7@gmail.com>
2022-03-14 21:02:00 -0600

    change for the windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -17,18 +17,22 @@ fetch( apiURL )
             document.querySelector('.weather-img > img').setAttribute('alt', desc);
             document.querySelector('#weather-api').textContent = desc;
             document.querySelector("#speed").textContent = data.wind.speed;
-            
+            cal_windchill();
         }
     });
+const cal_windchill = () =>{
 
 
-const t = parseInt(document.querySelector("#temp").textContent);
-const s = parseInt(document.getElementById("speed").textContent);
+    const t = parseInt(document.querySelector("#temp").textContent);
+    const s = parseInt(document.getElementById("speed").textContent);
+    console.log(t)
+    console.log(s)
+    if(t <= 50 && s > 3) {
+        const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
+        document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+    }
+    else {
+        document.querySelector("#windchill").innerHTML = "N/A"
+    }
 
-if(t <= 50 && s > 3) {
-    const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
-    document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
-}
-else {
-    document.querySelector("#windchill").innerHTML = "N/A"
 }
