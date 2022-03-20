f4a15830b7e5baf48828d48897567d667108a3cb
JaredMalan7 <jared.malan7@gmail.com>
2022-03-19 23:31:11 -0600

    update

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -25,8 +25,8 @@ const cal_windchill = () =>{
 
     const t = parseInt(document.querySelector("#temp").textContent);
     const s = parseInt(document.getElementById("speed").textContent);
-    console.log(t)
-    console.log(s)
+    // console.log(t)
+    // console.log(s)
     if(t <= 50 && s > 3) {
         const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
         document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
