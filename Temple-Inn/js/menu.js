51450e1535887946019b9f5b01a9bf49856e3378
JaredMalan7 <jared.malan7@gmail.com>
2022-04-06 13:20:37 -0600

    changes

--- Temple-Inn/js/menu.js
+++ Temple-Inn/js/menu.js
@@ -1,11 +1,12 @@
 
-const menubutton = document.querySelector('.menu');
-const mainnav = document.querySelector('.navigation');
-const closebutton = document.querySelector('#xbutton');
-const hamburger = document.querySelector('#hamburger');
+// const menubutton = document.querySelector('.menu');
+// const mainnav = document.querySelector('nav');
+// const navigation = document.querySelector(".navigaton")
+// const hidebutton = document.querySelector('#close-btn');
+// const hamburger = document.querySelector('#hamburger');
 
-menubutton.addEventListener('click', () => {
-    mainnav.classList.toggle('responsive');
-    closebutton.classList.toggle('hidden');
-    hamburger.classList.toggle('hidden');
-}, false);
+// menubutton.addEventListenver('click', () => {
+//     mainnav.classList.toggle('responsive');
+//     hidebutton.classList.toggle('hidden');
+//     hamburger.classList.toggle('hidden');
+// }, false);
--- Temple-Inn/js/temples.js
+++ Temple-Inn/js/temples.js
@@ -1,28 +1,28 @@
-const jasonData= "./js/temples.json"
+// const jasonData= "./js/temples.json"
 
-fetch(jasonData)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject){
-        const temples = jsonObject['temples'];
-    //    temple.forEach(displayTemples)
-        const filteredTemples = temples.filter((temple) => {
-            return temple.spotlight == "Featured";
-        });
+// fetch(jasonData)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (jsonObject){
+//         const temples = jsonObject['temples'];
+//     //    temple.forEach(displayTemples)
+//         const filteredTemples = temples.filter((temple) => {
+//             return temple.spotlight == "Featured";
+//         });
 
-        let firstTemple = Math.floor(Math.random()*filteredTemples.lenght);
-        let firstTemple = filteredTemples[i];
+//         let firstTemple = Math.floor(Math.random()*filteredTemples.lenght);
+//         let firstTemple = filteredTemples[i];
 
-        let firstSpotlight = document.getElementById('.home-temples');
+//         let firstSpotlight = document.getElementById('.home-temples');
 
-        let firstImage = document.createElement('img');
-        firstImage.src = firstTemple.image;
-        firstImage.setAttribute('alt', firstTemple.name);
-        firstSpotlight.appendChild(firstImage);
+//         let firstImage = document.createElement('img');
+//         firstImage.src = firstTemple.image;
+//         firstImage.setAttribute('alt', firstTemple.name);
+//         firstSpotlight.appendChild(firstImage);
 
-        let firstName = document.createElement('p');
-        firstName.src = firstName.name;
-        firstName.appendChild(firstName);
+//         let firstName = document.createElement('p');
+//         firstName.src = firstName.name;
+//         firstName.appendChild(firstName);
         
-    });
+//     });
