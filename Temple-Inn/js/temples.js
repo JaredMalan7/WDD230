fb3c26e01fe2cd988d3dd0ebcd8b8b31700f0f3e
JaredMalan7 <jared.malan7@gmail.com>
2022-04-05 23:48:29 -0600

    js

--- Temple-Inn/js/temples.js
+++ Temple-Inn/js/temples.js
@@ -4,9 +4,19 @@ fetch(jasonData)
     .then(function (response) {
         return response.json();
     })
-    .then(functoin (jsonObject){
+    .then(function (jsonObject){
         const temples = jsonObject['temples'];
+    //    forEach.temple in temples(displayTemples)
         const filteredTemples = temples.filter((temple) => {
-            return temple.
-        })
+            return temple.spotlight == "Featured";
+        });
+
+        let first = Math.floor(Math.random()*filteredTemples.lenght);
+        let firstTemple = filteredTemples[i];
+
+        var firstSpotlight = document.getElementById('.home-temples');
+
+        let firstImage = document.createElement('img');
+        firstImage.src = firstTemple.image;
+        firstImage.setAttribute('alt', firstTemple.name);
     })
