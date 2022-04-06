4bc03ded173c6e5e48523e2e62481ba71218577c
JaredMalan7 <jared.malan7@gmail.com>
2022-04-06 00:59:51 -0600

    Jason File Update

--- Temple-Inn/js/temples.js
+++ Temple-Inn/js/temples.js
@@ -11,12 +11,18 @@ fetch(jasonData)
             return temple.spotlight == "Featured";
         });
 
-        let first = Math.floor(Math.random()*filteredTemples.lenght);
+        let firstTemple = Math.floor(Math.random()*filteredTemples.lenght);
         let firstTemple = filteredTemples[i];
 
-        var firstSpotlight = document.getElementById('.home-temples');
+        let firstSpotlight = document.getElementById('.home-temples');
 
         let firstImage = document.createElement('img');
         firstImage.src = firstTemple.image;
         firstImage.setAttribute('alt', firstTemple.name);
-    })
+        firstSpotlight.appendChild(firstImage);
+
+        let firstName = document.createElement('p');
+        firstName.src = firstName.name;
+        firstName.appendChild(firstName);
+        
+    });
