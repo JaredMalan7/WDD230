a904bc8f181e568df253306b9acb2c3b9eb08445
JaredMalan7 <jared.malan7@gmail.com>
2022-04-06 13:53:59 -0600

    f

--- Temple-Inn/js/menu.js
+++ Temple-Inn/js/menu.js
@@ -1,12 +1,11 @@
 
-// const menubutton = document.querySelector('.menu');
-// const mainnav = document.querySelector('nav');
-// const navigation = document.querySelector(".navigaton")
-// const hidebutton = document.querySelector('#close-btn');
-// const hamburger = document.querySelector('#hamburger');
+const menubutton = document.querySelector('.menu');
+const mainnav = document.querySelector('.navigation');
+const closebutton = document.querySelector('.hidden');
+const hamburger = document.querySelector('#hamburger');
 
-// menubutton.addEventListenver('click', () => {
-//     mainnav.classList.toggle('responsive');
-//     hidebutton.classList.toggle('hidden');
-//     hamburger.classList.toggle('hidden');
-// }, false);
+menubutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+    closebutton.classList.toggle('hidden');
+    hamburger.classList.toggle('hidden');
+}, false);
