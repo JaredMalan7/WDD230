a13a654f8c8c62302badf6a919fb0ede5ae5f453
JaredMalan7 <jared.malan7@gmail.com>
2022-04-06 19:17:25 -0600

    services

--- Temple-Inn/js/temples.js
+++ Temple-Inn/js/temples.js
@@ -1,15 +1,3 @@
-// temples": [
-//     {
-//         "name":     "Mount Timpanogos, Utah, Temple",
-//         "address":  "742 N 900 E, American Fork, UT 84003",
-//         "image":    "./temples/timpanogos-temple.webp",
-//         "phone":    "+1 (801) 763-4540",    
-//         "services": ["Clothing rental available", "No cafeteria available", "No patron housing available", "Distribution center nearby"],
-//         "ordinances":   "By appointment only.",
-//         "sessions": "By appointment only.",
-//         "closures": ["Saturday, 2 April 2022","Monday, 9 May 2022 - Monday, 23 May 2022","Saturday, 1 October 2022","Monday, 7 November 2022 - Tuesday, 22 November 2022","Wednesday, 23 November 2022 - Thursday, 24 November 2022","Saturday, 24 December 2022","Saturday, 31 December 2022"],
-//         "history":  ["3 October 1992 - Announced","9 October 1993 - Groundbreaking","13 October 1996 - Dedicated"],
-//         "spotlight": "Featured"
 
 //https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Statements/async_function
 
@@ -37,6 +25,11 @@ async function getTemples() {
         const templeImage = document.createElement('img');
         const templeNameSpan = document.createElement('span')
         const templeServicesList = document.createElement('ul');
+        // console.log(templeslist);
+        templeServicesList.textContent = `Services:`;
+        
+        
+
         
         if(Array.isArray(temple.services)) {
             temple.services.forEach(service => {
@@ -54,6 +47,10 @@ async function getTemples() {
         templeImage.src = temple.image
         templeImage.className = 'temple-image'
         templeNameSpan.textContent = temple.name
+        
+
+
+
 
         // li append all children
         templeListElement.appendChild(templeImage)
