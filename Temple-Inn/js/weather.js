358ca12f2af69a5a0c40d0ffe716096866dcf5cb
JaredMalan7 <jared.malan7@gmail.com>
2022-04-07 23:59:18 -0600

    weather

--- Temple-Inn/js/weather.js
+++ Temple-Inn/js/weather.js
@@ -1,18 +1,34 @@
 const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=38.9807&lon=-77.1003&units=imperial&appid=c07521f35ad51dfde07c36d1d8632c37';
 
 
-fetch(apitURL)
+fetch(apiURL)
     .then( response => response.json())
     .then( jsObject => {
         const iconSrc = `https://openweathermap.org/img/w/${jsObject.current.weather[0].icon}.png`;
         const desc = jsObject.current.weather[0].description;
 
-        document.querySelector('.weather-image > img').textContent = iconSrc;
-        document.querySelector('#weather-img').setAttribute('src', iconsrc);
-        document.querySelector('weather-img > img').setAttribute('alt', desc)
-        document.querySelector('#temp').textContent = data.main.temp.toFixed(1);
-        document.querySelector('#speed').textContent = data.wind.speed;
 
+        document.querySelector('.weather-img > img').setAttribute('src', iconSrc);
+        document.querySelector('.weather-img > img').setAttribute('alt', desc)
+        document.querySelector('#temp').textContent = jsObject.main.temp.toFixed(1);
+        document.querySelector('#speed').textContent = jsObject.wind.speed;
 
-
+        cal_windchill();
     });
+
+    const cal_windchill = () =>{
+
+
+        const t = parseInt(document.querySelector("#temp").textContent);
+        const s = parseInt(document.getElementById("speed").textContent);
+        // console.log(t)
+        // console.log(s)
+        if(t <= 50 && s > 3) {
+            const windchill = 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16)
+            document.querySelector("#windchill").innerHTML = `${Math.round(windchill)}&#176;`;
+        }
+        else {
+            document.querySelector("#windchill").innerHTML = "N/A"
+        }
+    
+    }
